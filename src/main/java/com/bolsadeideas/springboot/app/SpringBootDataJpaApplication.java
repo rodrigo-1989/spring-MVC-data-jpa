51402610b64a9f2a8bf2791a024d44e33c01b46e
Julio <rodrigo_l321@hotmail.com>
2020-11-13 00:27:34 -0600

    Subir fotos del cliente & clase UploadFileService

--- src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -1,13 +1,26 @@
 package com.bolsadeideas.springboot.app;
 
+import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootDataJpaApplication {
 
+
+//Se implementa CommandLineRunner para asi crear la carpeta de uploads cada vez que se
+//arranca el servido para asi eliminar todas las fotos que nos quedan cuando bajamos el servidor
+@SpringBootApplication
+public class SpringBootDataJpaApplication implements CommandLineRunner {
+    @Autowired
+    IUploadFileService uploadFileService;
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDataJpaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        uploadFileService.deleteAll();
+        uploadFileService.init();
+    }
 }
--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,20 +1,29 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
 import com.bolsadeideas.springboot.app.util.paginator.PageRender;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Map;
 
 @Controller
@@ -23,12 +32,38 @@ public class ClienteController {
     @Autowired
     private IClienteService clienteService;
 
+    @Autowired
+    private IUploadFileService uploadFileService;
+
+    //Este siguiente metodo es una manera diferente de subir y ver la foto del cliente
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String filename){
+
+        Resource recurso = null;
+        try {
+            recurso = uploadFileService.load(filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attacht; filename=\""+ recurso.getFilename() +"\"" ).body(recurso);
+    }
+    @GetMapping(value = "/ver/{id}")
+    public String ver (@PathVariable (value = "id")Long id,Map<String,Object> modelo,RedirectAttributes flash ){
+        Cliente cliente = clienteService.findOne(id);
+        if (cliente == null){
+            flash.addFlashAttribute("error","El cliente no existe en la base de datos");
+            return "redirect:/listar";
+        }
+        modelo.put("cliente",cliente);
+        modelo.put("titulo","Detalles del cliente: "+ cliente.getNombre());
+        return "ver";
+    }
 
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model) {
 
         Pageable pageRequest = PageRequest.of(page, 4);
-
         Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
         PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
@@ -66,11 +101,27 @@ public class ClienteController {
 
     @RequestMapping(value = "/form",method = RequestMethod.POST)
     public String guardar(@Valid Cliente cliente, BindingResult result, Model modelo,
-                          RedirectAttributes flash, SessionStatus status){
+                          @RequestParam ("file")MultipartFile foto, RedirectAttributes flash, SessionStatus status){
 
         if(result.hasErrors()){
             modelo.addAttribute("titulo","Formulario de cliente");
             return "form";
+        }
+        if ( !foto.isEmpty()){
+            if (cliente.getId() != null
+                    && cliente.getId() > 0
+                    && cliente.getFoto() != null
+                    && cliente.getFoto().length() > 0) {
+                uploadFileService.delete(cliente.getFoto());
+            }
+            String uniqueFileName = null;
+            try {
+                uniqueFileName = uploadFileService.copy(foto);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            flash.addFlashAttribute("info","Has subido correctamente '"+uniqueFileName+ "'");
+            cliente.setFoto(uniqueFileName);
         }
         String mensajeFlash = (cliente.getId() != null ) ? "Cliente editado con exito" : "Cliente creado con exito";
         clienteService.save(cliente);
@@ -78,11 +129,19 @@ public class ClienteController {
         flash.addFlashAttribute("success",mensajeFlash);
         return "redirect:listar";
     }
+
     @RequestMapping(value = "/eliminar/{id}")
     public String eliminar (@PathVariable ( value = "id" )Long id,RedirectAttributes flash){
         if (id > 0){
+            Cliente cliente = clienteService.findOne(id);
             clienteService.delete(id);
             flash.addFlashAttribute("success","Cliente eliminado con exito!!");
+
+
+                if ( uploadFileService.delete(cliente.getFoto()) ){
+                    flash.addFlashAttribute("info","Foto "+cliente.getFoto()+" eliminada con exito");
+                }
+
         }
         return "redirect:/listar";
     }
